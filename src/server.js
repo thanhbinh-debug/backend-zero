a292aa55c97c2ece7cdac25359972e40f1a36cf7
thanhbinh <thanhbinh2limh6@gmail.com>
2025-11-02 18:40:05 +0700

    change 3

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require("express"); // commonjs
+const path = require("path");
 const app = express(); // app express
 const port = 8080; // prot
 
+// config template engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 // khai báo router
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -11,6 +15,10 @@ app.get("/abc", (req, res) => {
   res.send("Check");
 });
 
+app.get("/kakarot", (req, res) => {
+  // res.send("Check");
+  res.render("sample.ejs");
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
